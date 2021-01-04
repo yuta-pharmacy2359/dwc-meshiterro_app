3bd0e9224d933bceddf2b254db9ad04c6b9fa72e
Yuta <yuta.shogai@gmail.com>
2021-01-04 11:29:18 +0000

    [Updated] meshiterro 5-3 '画像投稿モデル作成、1:N実装、画像投稿・投稿一覧画面・投稿詳細画面作成、投稿削除機能実装、ログイン後遷移先変更'

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '72da66b00bee3854c9c999f794aa59c3e2bc46d72b395ac7c950f913c9488052ba9471640532c5ddd7f14873b290e82e293dacda245ec5793b9bf8f661a43873'
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
-
+  root "post_images#index"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
