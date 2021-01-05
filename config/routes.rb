c7996782356cbca3a65f6faf419f99ecf347b9c3
Yuta <yuta.shogai@gmail.com>
2021-01-05 11:50:36 +0000

    [Updated] meshiterro 5-4 'コメント機能の実装、いいね機能の実装、ページング機能の実装'

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   root "post_images#index"
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
 end
